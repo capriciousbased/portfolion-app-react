0df3b63aedbe64103af17f881b956de3fd0e581b
capriciousbased <mdanisulhoquekhan@gmail.com>
2021-03-06 23:18:10 +0100

    edited Homepage

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ import SocialLinks from "./SocialLinks";
 export default function Header() {
   return (
     <div className="home">
-      <h1 className="home-headline">I'm Anisul Hoque Khan.</h1>
+      <h1 className="home-headline">Hello, I'm Anis</h1>
       <h3 className="home-text">
         I'm a Berlin based <span>Frontend Developer</span>. After 12 years of
         creating adventures for others,it's time to start the next stage of my
