66e6fa3ed6930c504f87d1bb630a94200542596d
capriciousbased <mdanisulhoquekhan@gmail.com>
2022-01-18 13:37:44 +0100

    update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,9 @@ import "../App.css";
 const Footer = () => {
   return (
     <div className="footerStyle">
-      <span className="copy-right">&copy; 2021 Md Anisul H Khan</span>
+      <span className="copy-right">
+        &copy;{new Date().getFullYear()} Md Anisul H Khan
+      </span>
     </div>
   );
 };
